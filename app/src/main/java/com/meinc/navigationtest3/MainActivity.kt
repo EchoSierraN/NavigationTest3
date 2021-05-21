3040aca54aae6817f5d6628b1970b185b4d97dda
Elias Shemsu <echosierran@gmail.com>
2021-05-21 22:49:18 +0300

    Added single actionbar for all screens on MainActivity.kt, used passed arguments to rename actionbar title

--- app/src/main/java/com/meinc/navigationtest3/MainActivity.kt
+++ app/src/main/java/com/meinc/navigationtest3/MainActivity.kt
@@ -2,10 +2,30 @@ package com.meinc.navigationtest3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.meinc.navigationtest3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment= supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        navController= navHostFragment!!.findNavController()
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
