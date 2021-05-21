af41d903391252a9a646bf38d8931abfd20549ab
Elias Shemsu <echosierran@gmail.com>
2021-05-21 23:16:08 +0300

    Added options menu, Handled options menu click events Added global action to TermsFragment.kt destination in nav_graph.xml

--- app/src/main/java/com/meinc/navigationtest3/MainActivity.kt
+++ app/src/main/java/com/meinc/navigationtest3/MainActivity.kt
@@ -2,9 +2,12 @@ package com.meinc.navigationtest3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.meinc.navigationtest3.databinding.ActivityMainBinding
 
@@ -14,14 +17,32 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment= supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-        navController= navHostFragment!!.findNavController()
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        navController = navHostFragment!!.findNavController()
 
         setSupportActionBar(binding.toolbar)
         setupActionBarWithNavController(navController)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.termsAndConditions) {
+            val action = NavGraphDirections.actionGlobalTermsFragment()
+            navController.navigate(action)
+            true
+        } else {
+            //If clicked menu item id is similar to a fragment destination in the nav controller,
+            //navigation will happen. Otherwise, the default super function will be executed.
+            item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        }
 
     }
 
---
+++ app/src/main/java/com/meinc/navigationtest3/TermsFragment.kt
@@ -0,0 +1,6 @@
+package com.meinc.navigationtest3
+
+import androidx.fragment.app.Fragment
+
+class TermsFragment: Fragment(R.layout.fragment_terms) {
+}
