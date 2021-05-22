9b19a1401addea3588594c17ec647cca8923a79a
Elias Shemsu <echosierran@gmail.com>
2021-05-22 22:56:56 +0300

    Added DeepLinking to the Login

--- app/src/main/java/com/meinc/navigationtest3/LoginFragment.kt
+++ app/src/main/java/com/meinc/navigationtest3/LoginFragment.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.meinc.navigationtest3.databinding.FragmentLoginBinding
 import com.meinc.navigationtest3.databinding.FragmentWelcomeBinding
 
 class LoginFragment: Fragment(R.layout.fragment_login) {
     private var _binding: FragmentLoginBinding? = null
+    private val args: LoginFragmentArgs by navArgs()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -33,6 +35,11 @@ class LoginFragment: Fragment(R.layout.fragment_login) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val usernameDeepLink= args.username
+        //use setText() instead of the .text function here. Otherwise you'll get a mismatch
+        binding.editTextUsername.setText(usernameDeepLink)
+
         binding.buttonConfirm.setOnClickListener {
             val username= binding.editTextUsername.text.toString()
             val password= binding.editTextPassword.text.toString()
